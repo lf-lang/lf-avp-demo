1b03658d63c12823cafabc2b5746656d2a8185c0
Hou Seng Wong <stevenhouseng@gmail.com>
2022-03-14 17:19:37 -0700

    behavior planner compiles but segfaults due to unset parameter in yaml

--- src/include/constants.hpp
+++ src/include/constants.hpp
@@ -11,9 +11,9 @@ const std::string AVP_DEMO_PARAM_PATH = "src/launch/autoware_demos/param/avp";
 const std::string PC_FILTER_TRANSFORM_PARAM = "point_cloud_filter_transform.param.yaml";
 const std::string MPC_CONTROLLER_PARAM = "mpc_controller.param.yaml";
 const std::string NDT_MAP_PUBLISHER_PARAM = "map_publisher.param.yaml";
+const std::string BEHAVIOR_PLANNER_PARAM = "behavior_planner.param.yaml";
 
 // AVP Demo parameters
-const std::string BEHAVIOR_PLANNER_PARAM = "behavior_planner.param.yaml";
 const std::string EUCLIDEAN_CLUSTER_DETECTOR_PARAM = "euclidean_cluster.param.yaml";
 const std::string LANE_PLANNER_PARAM = "lane_planner.param.yaml";
 const std::string LANELET2_MAP_PROVIDER_PARAM = "lanelet2_map_provider.param.yaml";
