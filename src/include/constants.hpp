925044c96f331c9dea59b14ffedfa687c930515c
Soroosh129 <soroosh129@gmail.com>
2022-03-16 01:44:54 -0500

    Adding back nodeOptions for the MPC Controller (still segfaults)

--- src/include/constants.hpp
+++ src/include/constants.hpp
@@ -9,7 +9,7 @@ const std::string AVP_DEMO_PARAM_PATH = "src/launch/autoware_demos/param/avp";
 
 // Launch parameters
 const std::string PC_FILTER_TRANSFORM_PARAM = "point_cloud_filter_transform.param.yaml";
-const std::string MPC_CONTROLLER_PARAM = "mpc_controller.param.yaml";
+const std::string MPC_CONTROLLER_PARAM = "mpc_sim.param.yaml";
 const std::string NDT_MAP_PUBLISHER_PARAM = "map_publisher.param.yaml";
 const std::string BEHAVIOR_PLANNER_PARAM = "behavior_planner.param.yaml";
 const std::string OFF_MAP_OBSTACLES_FILTER_PARAM = "off_map_obstacles_filter.param.yaml";
