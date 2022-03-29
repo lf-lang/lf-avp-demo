e467205b37e3bde7080a1bcc52435302edb2bfbd
Steven <stevenhouseng@gmail.com>
2022-03-29 10:59:39 -0700

    adjust for 'Received route' message

--- extract-timestamps.py
+++ extract-timestamps.py
@@ -1,28 +1,38 @@
 import sys
-if len(sys.argv) != 2:
-    print("ERROR: must specify log file name as argument")
-    exit(1)
-f_name = sys.argv[1]
+import glob
+# if len(sys.argv) != 2:
+#     print("ERROR: must specify log file name as argument")
+#     exit(1)
+# f_name = sys.argv[1]
 
-trajectories = []
-kinematic = []
+f_names = glob.glob("lf-data/log*.txt")
 
-with open(f_name, "r") as f:
-    for line in f.read().split("\n"):
-        if "(YOLO)>>> MPC received" in line:
-            trajectories.append(line)
-        elif "(YOLO)>>> Published kinematic" in line:
-            kinematic.append(line)
+first = 5
+total_diffs = []
 
-diffs = []
-for i, j in zip(trajectories, kinematic):
-    try:
-        i_l = i.find("{")
-        traj_t = int(i[i_l + 1:i.find("}", i_l + 1)])
-        j_l = j.find("{")
-        kine_t = int(j[j_l + 1:j.find("}", j_l + 1)])
-        diffs.append((traj_t - kine_t) // 1_000_000)
-    except:
-        pass
+for f_name in f_names:
+    with open(f_name, "r") as f:
+        print(f_name)
+        trajectories = []
+        kinematic = []
+        seen_route = False
+        for line in f.read().split("\n"):
+            if not seen_route and "Received route" in line:
+                seen_route = True
+            if seen_route and "(YOLO)>>> M" in line:
+                trajectories.append(line)
+            elif seen_route and "(YOLO)>>> P" in line:
+                kinematic.append(line)
+        diffs = []
+        for i, j in zip(trajectories[1:first+1], kinematic[1:first+1]):
+            i_l = i.find("{")
+            traj_t = int(i[i_l + 1:i.find("}", i_l + 1)])
+            j_l = j.find("{")
+            kine_t = int(j[j_l + 1:j.find("}", j_l + 1)])
+            diffs.append((traj_t - kine_t) // 1_000_000)
+        print(diffs)
+        total_diffs += diffs
 
-print(diffs)
+with open("results.txt", "w") as f:
+    for i in total_diffs:
+        f.write(str(i) + "\n")
